24d771d9dcb5eaff3317653784fb2bb60209e949
Garron Giesbrecht <garronvgj11@gmail.com>
2020-04-06 01:55:12 -0500

    Got the log fragment working

--- app/src/main/java/com/example/finalproject/LogFragment.java
+++ app/src/main/java/com/example/finalproject/LogFragment.java
@@ -1,18 +1,65 @@
 package com.example.finalproject;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LogFragment extends Fragment {
+    private LogFragmentListener listener;
+    private NumberPicker numpHours, numpMinutes;
+    private Button btnLog;
+
+    public interface LogFragmentListener {
+        void sendToActivity(int hours, int minutes);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmant_log, container, false);
+        View v = inflater.inflate(R.layout.fragmant_log, container, false);
+
+        btnLog = v.findViewById(R.id.btnLog);
+        numpHours = v.findViewById(R.id.numHours);
+        numpMinutes = v.findViewById(R.id.numMinutes);
+
+        numpHours.setMinValue(0);
+        numpHours.setMaxValue(24);
+        numpMinutes.setMinValue(0);
+        numpMinutes.setMaxValue(59);
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hours = numpHours.getValue();
+                int minutes = numpMinutes.getValue();
+                listener.sendToActivity(hours, minutes);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof LogFragmentListener) {
+            listener = (LogFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement LogFragmentListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -9,10 +9,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -20,11 +16,8 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LogFragment.LogFragmentListener {
     private Toolbar toolbar;
-    EditText etUpdate;
-    private NumberPicker numpHours, numpMinutes;
-    private Button btnLog, btnShow, btnUpdate, btnDelete;
     BottomNavigationView bottomNav;
     private DatabaseHelper dbHelper;
 
@@ -37,37 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         dbHelper = new DatabaseHelper(this);
-        this.setupControls();
+        setupBottomNav();
+
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragmentContainer, new LogFragment()).commit();
     }
 
-    private void setupControls() {
-        btnLog = (Button) findViewById(R.id.btnLog);
-        btnShow = (Button) findViewById(R.id.btnShow);
-        btnUpdate = (Button) findViewById(R.id.btnUpdate);
-        btnDelete = (Button) findViewById(R.id.btnDelete);
-        numpHours = (NumberPicker) findViewById(R.id.numHours);
-        numpMinutes = (NumberPicker) findViewById(R.id.numMinutes);
-        etUpdate = (EditText) findViewById(R.id.etUpdate);
-        bottomNav = (BottomNavigationView) findViewById(R.id.bottomNav);
+    @Override
+    public void sendToActivity(int hours, int minutes) {
+        logTime(hours, minutes);
+    }
 
-        btnShow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showData();
-            }
-        });
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateData();
-            }
-        });
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteData();
-            }
-        });
+    private void setupBottomNav() {
+        bottomNav = (BottomNavigationView) findViewById(R.id.bottomNav);
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -89,33 +64,15 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-        numpHours.setMinValue(0);
-        numpHours.setMaxValue(24);
-        numpMinutes.setMinValue(0);
-        numpMinutes.setMaxValue(59);
     }
 
-    public void logTime(View v) {
-        int hours = numpHours.getValue();
-        int minutes = numpMinutes.getValue();
+    public void logTime(int hours, int minutes) {
         boolean isInserted = dbHelper.insert(hours, minutes);
         if (isInserted) {
             Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(MainActivity.this, "Data not inserted", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    private void updateData() {
-        dbHelper.update(
-            Integer.parseInt(etUpdate.getText().toString()),
-            numpHours.getValue(),
-            numpMinutes.getValue());
-    }
-
-    private void deleteData() {
-        dbHelper.delete(Integer.parseInt(etUpdate.getText().toString()));
     }
 
     private void showData() {
