a61c17da773de582d845ae1d92528fd30bde2e7e
Garron Giesbrecht <garronvgj11@gmail.com>
2020-04-06 16:31:53 -0500

    changed number pickers to spinners

--- app/src/main/java/com/example/finalproject/LogFragment.java
+++ app/src/main/java/com/example/finalproject/LogFragment.java
@@ -5,16 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class LogFragment extends Fragment {
     private LogFragmentListener listener;
-    private NumberPicker numpHours, numpMinutes;
+    private Spinner spHours, spMinutes;
     private Button btnLog;
 
     public interface LogFragmentListener {
@@ -26,25 +31,45 @@ public class LogFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmant_log, container, false);
 
-        btnLog = v.findViewById(R.id.btnLog);
-        numpHours = v.findViewById(R.id.numHours);
-        numpMinutes = v.findViewById(R.id.numMinutes);
+        btnLog = (Button) v.findViewById(R.id.btnLog);
+        spHours = (Spinner) v.findViewById(R.id.spHours);
+        spMinutes = (Spinner) v.findViewById(R.id.spMinutes);
 
-        numpHours.setMinValue(0);
-        numpHours.setMaxValue(24);
-        numpMinutes.setMinValue(0);
-        numpMinutes.setMaxValue(59);
+        setupSpinners(v);
 
         btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int hours = numpHours.getValue();
-                int minutes = numpMinutes.getValue();
+                int hours = Integer.parseInt(spHours.getSelectedItem().toString());
+                int minutes = Integer.parseInt(spMinutes.getSelectedItem().toString());
                 listener.sendToActivity(hours, minutes);
             }
         });
 
         return v;
+    }
+
+    private void setupSpinners(View v) {
+        ArrayList<String> hoursList = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            hoursList.add(String.valueOf(i));
+        }
+
+        ArrayList<String> minutesList = new ArrayList<>();
+        for (int i = 0; i < 60; i++) {
+            minutesList.add(String.valueOf(i));
+        }
+
+        ArrayAdapter<String> hoursAdapter = new ArrayAdapter<>(
+            v.getContext(), R.layout.spinner, hoursList);
+        ArrayAdapter<String> minutesAdapter = new ArrayAdapter<>(
+            v.getContext(), R.layout.spinner, minutesList);
+
+        hoursAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minutesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spHours.setAdapter(hoursAdapter);
+        spMinutes.setAdapter(minutesAdapter);
     }
 
     @Override
